716bf567241d05464f0198b84ff1a89e7d309c56
jonathankoubi-jpg <jonathan.koubi@gmail.com>
2025-08-11 15:25:56 +0300

    Delete app/api/upload directory

--- app/api/upload/route.ts
+++
@@ -1,8 +0,0 @@
-export const runtime = 'nodejs';
-
-export async function POST() {
-  return new Response(JSON.stringify({ ok: true, note: 'route minimale' }), {
-    headers: { 'content-type': 'application/json' },
-    status: 200,
-  });
-}
